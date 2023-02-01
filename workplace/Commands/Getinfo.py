4ed0f152cc902a4913483a46bc7bca2acb6fda1f
lankryf <lankrf@gmail.com>
2023-02-01 19:40:52 +0200

    Getinfo command

---
+++ workplace/Commands/Getinfo.py
@@ -0,0 +1,48 @@
+# Copyright 2023 LankryF
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from workplace.Commands.Common.CommandFather import *
+
+class Getinfo(Father):
+
+    flags = ()
+    hints = {
+        None: (),
+        'char': ('characters_names',),
+        'team': ('teams_names',)
+    }
+
+    @staticmethod
+    def body(wp:Workplace, cmd:Command):
+        funcs = {
+            "char": (
+                wp.bar.characterIdByName,
+                wp.bar.characterAllNames
+            ),
+            "team": (
+                wp.bar.teamIdByName,
+                wp.bar.teamAllNames
+            )
+        }
+
+        objectId = funcs[cmd.mode][0](cmd.args[0])
+        if objectId is None:
+            wp.hog.err(f'{cmd.mode}\'s name "{cmd.args[0]}" wasn\'t found!'.capitalize())
+            return
+
+        
+        names = funcs[cmd.mode][1](objectId)
+
+        wp.hog.info(f"{cmd.mode}'s ID = {objectId}")
+        wp.hog.info(f"{cmd.mode}'s names ({len(names)}):\n    {names}")
